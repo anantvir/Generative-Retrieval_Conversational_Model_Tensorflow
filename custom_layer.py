62d42e37e6fb201a89b87890361c3e818f2a30d9
Anantvir Singh <anantvir91@gmail.com>
2020-04-26 03:42:54 -0400

    custom layer

---
+++ custom_layer.py
@@ -0,0 +1,19 @@
+from tensorflow.keras import layers
+import tensorflow as tf
+
+
+class CustomLayer(layers.Layer):
+
+  def __init__(self, units=256, input_dim=256):
+    super(CustomLayer, self).__init__()
+    M_init = tf.random_normal_initializer()
+    self.M = tf.Variable(initial_value=M_init(shape=(input_dim, units),dtype='float32'),trainable=True)
+
+  def call(self, inputs):
+    return tf.matmul(inputs, self.M)
+
+# x = tf.ones((32, 256))
+# custom_layer = CustomLayer(256, 256)
+# y = custom_layer(x)
+# print(y)
+# print("Hello")
